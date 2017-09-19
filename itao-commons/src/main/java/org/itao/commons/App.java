9cae203d6b9766eb5cfe7c2e2f115990a0da1ef0
wangde <wangde1662@dingtalk.com>
2017-09-19 16:10:22 +0800

    itao

---
+++ itao-commons/src/main/java/org/itao/commons/App.java
@@ -0,0 +1,13 @@
+package org.itao.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
